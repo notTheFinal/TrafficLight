927a27c70a0ee807d4bef90ecb42ad7e89960c57
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-08-13 11:42:22 +0800

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Александр on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
